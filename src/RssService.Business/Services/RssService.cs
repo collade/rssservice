eb1222e240b8b8373057b9f9931a1ee6aa794f2f
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-08-12 20:56:15 +0300

    test setup and the rssservice constructer work

--- src/RssService.Business/Services/RssService.cs
+++ src/RssService.Business/Services/RssService.cs
@@ -1,7 +1,65 @@
 ﻿namespace RssService.Business.Services
 {
-    public class RssService
-    {
+    using global::RssService.Business.Entities;
+    using global::RssService.Business.Repos;
 
+    public class RssService : IRssService
+    {
+        private readonly IEntityRepository<Organization> organizationRepo;
+        private readonly IEntityRepository<DistinctRssAddress> distinctRssAddressRepo;
+        private readonly IEntityRepository<RssAddress> rssAddressRepo;
+        private readonly IEntityRepository<RssItem> rssItemRepo;
+
+        public RssService(
+            IEntityRepository<Organization> organizationRepo,
+            IEntityRepository<DistinctRssAddress> distinctRssAddressRepo,
+            IEntityRepository<RssAddress> rssAddressRepo,
+            IEntityRepository<RssItem> rssItemRepo)
+        {
+            this.organizationRepo = organizationRepo;
+            this.distinctRssAddressRepo = distinctRssAddressRepo;
+            this.rssAddressRepo = rssAddressRepo;
+            this.rssItemRepo = rssItemRepo;
+        }
+
+        public bool HasOrganization(string organizationId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AddOrganization(string organizationId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AddRss(string organizationId, string rss)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RemoveRss(string organizationId, string rss)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool GetRsses(string organizationId, string rss)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasRssForOrganization(string organizationId, string rss)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasDistinctRss(string keyword)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Run()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- src/RssService.Test/RssServiceTests.cs
+++ src/RssService.Test/RssServiceTests.cs
@@ -8,13 +8,37 @@ namespace RssService.Test
 {
     using NUnit.Framework;
 
+    using RssService.Business.Entities;
+    using RssService.Business.Repos;
+    using RssService.Business.Services;
+
     [TestFixture]
     public class RssServiceTests
     {
+        protected IEntityRepository<Organization> _organizationRepo;
+        protected IEntityRepository<DistinctRssAddress> _distinctRssAddressRepo;
+        protected IEntityRepository<RssAddress> _rssAddressRepo;
+        protected IEntityRepository<RssItem> _rssItemRepo;
+
+        protected IRssService _rssService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _organizationRepo = new EntityRepository<Organization>();
+            _distinctRssAddressRepo = new EntityRepository<DistinctRssAddress>();
+            _rssAddressRepo = new EntityRepository<RssAddress>();
+            _rssItemRepo = new EntityRepository<RssItem>();
+
+            _rssService = new RssService(_organizationRepo, _distinctRssAddressRepo, _rssAddressRepo, _rssItemRepo);
+        }
+
 
         [Test]
-        public void Should_save_rss_when_addrss_method_called() {
+        public void Should_save_rss_when_addrss_method_called()
+        {
 
         }
     }
+
 }
